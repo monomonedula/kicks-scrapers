cfff4c7ada31182543c3de3e9c1c5dc63883f37d
monomonedula <valh@tuta.io>
2019-12-04 22:27:55 +0200

    pipelines refactored

--- kicks_scraper/pipelines.py
+++ kicks_scraper/pipelines.py
@@ -8,43 +8,63 @@ import logging
 from functools import partial
 
 from elasticsearch.helpers import bulk
+from typing import Dict
 
-from dbhandling.indexing import es, update_script, EmptyBufferWriteException, \
+from dbhandling.indexing import es, EmptyBufferWriteException, \
     BufferOverflowException
 
 logger = logging.getLogger(__name__)
 
 
+class ItemsStorage:
+    def __init__(self, bulk, buffers: Dict[str, "ItemsBuffer"] = None, buffer_size=200):
+        self._buffers = buffers or {}
+        self._bulk = bulk
+        self._buff_size = buffer_size
+
+    def add(self, item, key):
+        buffer = self._buffers.setdefault(key, ItemsBuffer(self._buff_size))
+        if buffer.is_full():
+            updates = (item.get_bulk_update_dict() for item in buffer.retrieve())
+            self._bulk(updates)
+        buffer.add(item)
+
+    def close(self):
+        for buffer in self._buffers.values():
+            if not buffer.is_empty():
+                updates = (item.get_bulk_update_dict() for item in buffer.retrieve())
+                self._bulk(updates)
+
+
+class ItemsBuffer:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self._items = dict()
+
+    def __len__(self):
+        return len(self._items)
+
+    def is_full(self):
+        return len(self) >= self.max_size
+
+    def is_empty(self):
+        return len(self) == 0
+
+    def add(self, item):
+        if self.is_full():
+            raise BufferOverflowException('Unable to add item to buffer.'
+                                          ' Buffer is full')
+        self._items[item.meta.id] = item
+
+    def retrieve(self):
+        items = list(self._items.values())
+        self._items = {}
+        return items
+
+
 class KicksScraperPipeline:
-    class ItemsBuffer:
-        def __init__(self, max_size):
-            self.max_size = max_size
-            self._items = dict()
-
-        def __len__(self):
-            return len(self._items)
-
-        def is_full(self):
-            return len(self) >= self.max_size
-
-        def is_empty(self):
-            return len(self) == 0
-
-        def add(self, item):
-            if self.is_full():
-                raise BufferOverflowException('Unable to add item to buffer.'
-                                              ' Buffer is full')
-            self._items[item.meta.id] = item
-
-        def retrieve(self):
-            items = list(self._items.values())
-            self._items = {}
-            return items
-
     def __init__(self, bulk, buffer_size=200):
-        self.bulk = bulk
-        self.default_buffer_size = buffer_size
-        self.buffers = {}
+        self._storage = ItemsStorage(bulk, buffer_size=buffer_size)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -52,45 +72,8 @@ class KicksScraperPipeline:
         return cls(buffer_size=s.getint('DB_BUFFER_SIZE', 200),
                    bulk=partial(bulk, es))
 
-    def write_from_buffer(self, spider):
-        buffer = self.buffers[spider.name]
-        if buffer.is_empty():
-            raise EmptyBufferWriteException("Unable to write"
-                                            " from the buffer because"
-                                            " it is empty.")
-        items_batch = buffer.retrieve()
-        return self._write(items_batch)
-
-    def _write(self, items_batch):
-        updates = (item.get_bulk_update_dict() for item in items_batch)
-        return self.bulk(updates)
-
     def process_item(self, item, spider):
-        buffer = self._get_buffer(spider)
-        buffer.add(item.to_elastic())
-        if buffer.is_full():
-            self.write_from_buffer(spider)
-
-    def _get_buffer(self, spider):
-        if spider.name not in self.buffers:
-            size = getattr(spider, 'buffer_size', self.default_buffer_size)
-            buff = self.ItemsBuffer(size)
-            self.buffers[spider.name] = buff
-        return self.buffers[spider.name]
+        self._storage.add(item.to_elastic(), spider.name)
 
     def close_spider(self, spider):
-        for buffer in self.buffers.values():
-            if not buffer.is_empty():
-                self.write_from_buffer(buffer)
-
-
-class SessionedKicksScraperPipeline(KicksScraperPipeline):
-    def write_from_buffer(self, spider):
-        buff_size = self._buffer_size(spider)
-        # logger.info(f'Writing batch to elasticsearch. Batch size: {buff_size}')
-        result = super().write_from_buffer(spider)
-        spider.session.update_status(buff_size)
-        return result
-
-    def _buffer_size(self, spider):
-        return len(self.buffers[spider.name])
+        self._storage.close()
