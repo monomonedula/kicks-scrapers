eca66c29283de08ab236d82408749ecb46cf9be7
monomonedula <valh@tuta.io>
2019-12-06 12:58:32 +0200

    percolation implemented

--- dbhandling/elastic_models.py
+++ dbhandling/elastic_models.py
@@ -3,18 +3,27 @@ import logging
 from datetime import datetime, timezone
 
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import (Text, Document, Integer, Double, Keyword,
-                               Boolean, Float, Percolator, Q)
+from elasticsearch_dsl import (
+    Text,
+    Document,
+    Integer,
+    Double,
+    Keyword,
+    Boolean,
+    Float,
+    Percolator,
+    Search,
+)
 
 client = Elasticsearch()
 
-index_name = 'kicks'
+index_name = "kicks"
 
 logger = logging.getLogger(__name__)
 
 this_directory_path = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(this_directory_path, 'update_script.painless')) as scr:
+with open(os.path.join(this_directory_path, "update_script.painless")) as scr:
     update_script = scr.read()
 
 
@@ -29,22 +38,10 @@ class SneakerItem(Document):
     brand = Text()
     model = Text()
     colorway = Text()
-    item_id = Text(
-        fields={'keyword': Keyword()}
-    )
-    url = Text(
-        analyzer='simple',
-        fields={'keyword': Keyword()},
-        required=True,
-    )
-    img_url = Text(
-        analyzer='simple',
-        fields={'keyword': Keyword()}
-    )
-    telegram_img_id = Text(
-        analyzer='simple',
-        fields={'keyword': Keyword()}
-    )
+    item_id = Text(fields={"keyword": Keyword()})
+    url = Text(analyzer="simple", fields={"keyword": Keyword()}, required=True,)
+    img_url = Text(analyzer="simple", fields={"keyword": Keyword()})
+    telegram_img_id = Text(analyzer="simple", fields={"keyword": Keyword()})
     last_update = Double()
 
     new = Boolean()
@@ -59,39 +56,62 @@ class SneakerItem(Document):
 
     @property
     def descr(self):
-        return (self.name or
-                (self.brand + self.model + self.colorway)).upper()
+        return (self.name or (self.brand + self.model + self.colorway)).upper()
 
     def get_bulk_update_dict(self):
         d = self.to_dict(include_meta=True)
-        del d['_source']
-        d['_op_type'] = 'update'
-        d['script'] = self.get_update_script()
-        d['upsert'] = self.get_upsert_dict()
+        del d["_source"]
+        d["_op_type"] = "update"
+        d["script"] = self.get_update_script()
+        d["upsert"] = self.get_upsert_dict()
         return d
 
     def get_update_script(self):
         return {
-            'lang': 'painless',
-            'source': update_script,
-            'params': {
-                'sizes': list(self.sizes),
-                'price': self.price,
-                'new_update_time': get_time(),
-                'img_url': self.img_url,
-            }
+            "lang": "painless",
+            "source": update_script,
+            "params": {
+                "sizes": list(self.sizes),
+                "price": self.price,
+                "new_update_time": get_time(),
+                "img_url": self.img_url,
+            },
         }
 
     def get_upd_dict(self):
         pass
 
     def get_upsert_dict(self):
-        return {**self.to_dict(), 'new': True}
+        return {**self.to_dict(), "new": True}
+
+    def to_dict(self, include_meta=False, skip_empty=True):
+        # TODO: this may be inefficient on large document set,
+        #       consider implementing bulk percolation mechanism
+        data = super().to_dict(include_meta, skip_empty)
+        s = (
+            Search(index="test-percolator")
+            .query(
+                "percolate",
+                field="query",
+                index=self._get_index(),
+                document=self.to_dict(),
+            )
+            .extra(min_score=0.15)
+        )
+        # min_score value may be adjusted or removed along with .extra call
+        try:
+            hit = max(s, key=lambda h: h.meta.score)
+        except ValueError:
+            pass
+        else:
+            data["recommended"] = True
+            data["recommended_price_diff"] = data["price"] - hit.recommended_price
+        return data
 
 
 class RunRepeatItem(Document):
     class Index:
-        name = 'runrepeat'
+        name = "runrepeat"
         using = client
 
     name = Text()
@@ -104,15 +124,15 @@ class RunRepeatItem(Document):
 
     def get_bulk_update_dict(self):
         d = self.to_dict(include_meta=True)
-        d['_op_type'] = 'update'
-        d['doc'] = d.pop('_source')
-        d['doc_as_upsert'] = True
+        d["_op_type"] = "update"
+        d["doc"] = d.pop("_source")
+        d["doc_as_upsert"] = True
         return d
 
 
 class Recommendation(Document):
     class Index:
-        name = 'test-percolator'
+        name = "test-percolator"
 
     name = Text()
     brand = Text()
@@ -121,7 +141,7 @@ class Recommendation(Document):
     query = Percolator()
     recommended_price = Integer()
 
-
+# recommendation example construction:
 # Recommendation(
 #     _id="1234",
 #     recommended_price=100,
